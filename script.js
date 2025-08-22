27f040d704d2c548bb88507ca0ab33593a23c5b0
Esra hamed <s2212176087@ku.edu.kw>
2025-08-22 03:14:16 +0300

    done

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let name = prompt("enter your name ");
+
+let age = prompt("enter your age ");
+
+
+
+function checkAge(age){
+
+
+if (age >= 18){
+alert("hello " + name + " You can drive");
+
+}
+    
+else {
+alert("hello " + name + " You're too young to drive");
+
+}
+
+
+}
+
+let call = checkAge(age);
